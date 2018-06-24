29403db0b01679d2011f6ebba079efea778cfb16
Evil-Sphinx <filco9617@163.com>
2018-06-25 00:13:05 +0800

    2018年6月25日00:13:02

--- pat/1142.cpp
+++ pat/1142.cpp
@@ -6,6 +6,8 @@ const int MAXV = 201;
 
 int G[MAXV][MAXV] = { 0 };
 
+int Nv, Ne;
+
 //判断是否为Clique 
 bool judgeClique(vector<int> &v) {
 	int size = v.size();
@@ -17,8 +19,37 @@ bool judgeClique(vector<int> &v) {
 	return true;
 }
 
+//判断是否为MaxmialClique 
+bool judgeMaxmial(vector<int> &v) {
+	vector<int> tmp;
+	for (int i = 1; i <= Nv; i++) {
+		if (G[v[0]][i])	tmp.push_back(i);		//找出其中一个点所有相邻的点 
+	}
+	vector<int> cnt(Nv + 1);
+	for (int i = 1; i < v.size(); i++) {
+		for (int j = 0; j < tmp.size(); j++) {
+			if (G[v[i]][tmp[j]])		cnt[tmp[j]]++;
+		}
+	}
+	for (int i = 1; i < Nv; i++) {
+		if (cnt[i] == (v.size() - 1)) {
+
+			if (v.size() == 1 && tmp.size() == 1) {
+				return true;
+			}
+			else {
+				return false;
+			}
+
+		}
+
+	}
+
+	return true;
+}
+
 int main() {
-	int Nv, Ne, v1, v2;
+	int v1, v2;
 	scanf("%d %d", &Nv, &Ne);
 	for (int i = 1; i <= Ne; i++) {
 		scanf("%d %d", &v1, &v2);
@@ -34,10 +65,15 @@ int main() {
 			scanf("%d", &v[j]);
 		}
 		if (judgeClique(v)) {
-			cout << "Yes";
+			if (judgeMaxmial(v)) {
+				cout << "Yes" << endl;
+			}
+			else {
+				cout << "Not Maxmial" << endl;
+			}
 		}
 		else {
-			cout << "Not a Clique";
+			cout << "Not a Clique" << endl;
 		}
 
 	}
